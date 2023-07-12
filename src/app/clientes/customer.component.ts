2069ec735c019733ae2f6446daf697074f1fc597
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2023-07-12 13:06:47 -0300

    Actualizar customer.component.ts

--- src/app/clientes/customer.component.ts
+++ src/app/clientes/customer.component.ts
@@ -5,27 +5,31 @@ import { debounceTime } from 'rxjs/operators';
 
 import { Customer } from './customer';
 
-function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
-  const emailControl = c.get('email');
-  const confirmControl = c.get('confirmEmail');
+class EmailMatcher {
+  static match(c: AbstractControl): { [key: string]: boolean } | null {
+    const emailControl = c.get('email');
+    const confirmControl = c.get('confirmEmail');
 
-  if (emailControl.pristine || confirmControl.pristine) {
-    return null;
-  }
+    if (emailControl?.pristine || confirmControl?.pristine) {
+      return null;
+    }
 
-  if (emailControl.value === confirmControl.value) {
-    return null;
+    if (emailControl?.value === confirmControl?.value) {
+      return null;
+    }
+    return { match: true };
   }
-  return { match: true };
 }
 
-function ratingRange(min: number, max: number): ValidatorFn {
-  return (c: AbstractControl): { [key: string]: boolean } | null => {
-    if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
-      return { range: true };
-    }
-    return null;
-  };
+class RatingRangeValidator {
+  static range(min: number, max: number): ValidatorFn {
+    return (c: AbstractControl): { [key: string]: boolean } | null => {
+      if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
+        return { range: true };
+      }
+      return null;
+    };
+  }
 }
 
 @Component({
@@ -36,7 +40,7 @@ function ratingRange(min: number, max: number): ValidatorFn {
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
-  emailMessage: string;
+  emailMessage: string = '';
 
   get addresses(): FormArray {
     return this.customerForm.get('addresses') as FormArray;
@@ -56,19 +60,19 @@ export class CustomerComponent implements OnInit {
       emailGroup: this.fb.group({
         email: ['', [Validators.required, Validators.email]],
         confirmEmail: ['', Validators.required],
-      }, { validator: emailMatcher }),
+      }, { validator: EmailMatcher.match }),
       phone: '',
       notification: 'email',
      
       addresses: this.fb.array([this.buildAddress()])
     });
 
-    this.customerForm.get('notification').valueChanges.subscribe(
+    this.customerForm.get('notification')?.valueChanges.subscribe(
       value => this.setNotification(value)
     );
 
     const emailControl = this.customerForm.get('emailGroup.email');
-    emailControl.valueChanges.pipe(
+    emailControl?.valueChanges.pipe(
       debounceTime(1000)
     ).subscribe(
       value => this.setMessage(emailControl)
@@ -123,11 +127,10 @@ export class CustomerComponent implements OnInit {
   setNotification(notifyVia: string): void {
     const phoneControl = this.customerForm.get('phone');
     if (notifyVia === 'text') {
-      phoneControl.setValidators(Validators.required);
+      phoneControl?.setValidators(Validators.required);
     } else {
-      phoneControl.clearValidators();
+      phoneControl?.clearValidators();
     }
-    phoneControl.updateValueAndValidity();
+    phoneControl?.updateValueAndValidity();
   }
-
 }
